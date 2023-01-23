072900b1384db3c0ba220408bfdd3ae7bfead6c5
Joel <joeljohnvarghese2@gmail.com>
2023-01-23 19:03:47 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'e0b2bd40c3msh4eb54ca89076335p1fc8dcjsn3be93d605a23',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+
+const getWeather = (city) => {
+    cityName.innerHTML = city
+
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response)
+
+            cloud_pct.innerHTML = response.cloud_pct
+            temp.innerHTML = response.temp
+            feels_like.innerHTML = response.feels_like
+            humidity.innerHTML = response.humidity
+            min_temp.innerHTML = response.min_temp
+            max_temp.innerHTML = response.max_temp
+            wind_speed.innerHTML = response.wind_speed
+            wind_degrees.innerHTML = response.wind_degrees
+            sunrise.innerHTML = response.sunrise
+            sunset.innerHTML = response.sunset
+        })
+        .catch(err => console.error(err));
+}
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault()
+    getWeather(city.value)
+})
+
+getWeather(" New Delhi")
